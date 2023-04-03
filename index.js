8d8de34495963767252d05c9e993469a172ee894
Omar <Omarggm@gmail.com>
2023-04-02 21:02:24 -0700

    init commit added base code to work off

---
+++ index.js
@@ -0,0 +1,35 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer.prompt([
+    {
+        name: 'characters',
+        message: 'Enter up tp three characters',
+        type : 'input',
+        validate : function(input){
+            if (input.length > 3){
+                return 'you must use three characters or less';
+            }
+            return true;
+        }
+    },
+    {
+        name : 'textColor',
+        message : 'please input a color for the text',
+        type : 'input',
+    },
+    {
+        name : 'shape',
+        message : 'please select a shape',
+        input : 'list',
+        choices : ['circle', 'triangle', 'square']
+    },
+    {
+        name : 'shapeColor',
+        message : 'please input a color for the shape',
+        type : 'input',
+    },
+])
+.then((answers) =>{
+    console.log(answers);
+});
