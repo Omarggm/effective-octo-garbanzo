360448fb0fa96d8830a1c2740ca8fbe5bb8317c4
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-06-11 20:55:38 -0700

    added js for svg to work

--- index.js
+++ index.js
@@ -1,35 +1,98 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const SVG = require("svg.js");
 
-inquirer.prompt([
+async function generateLogo() {
+return inquirer
+  .prompt([
     {
-        name: 'characters',
-        message: 'Enter up tp three characters',
-        type : 'input',
-        validate : function(input){
-            if (input.length > 3){
-                return 'you must use three characters or less';
-            }
-            return true;
+      name: "characters",
+      message: "Enter up to three characters",
+      type: "input",
+      validate: function (input) {
+        if (input.length > 3) {
+          return "You must use three characters or less";
         }
+        return true;
+      },
     },
     {
-        name : 'textColor',
-        message : 'please input a color for the text',
-        type : 'input',
+      name: "textColor",
+      message: "Please select a color for the text",
+      type: "list",
+      choices: [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "purple",
+        "black",
+        "white",
+      ],
     },
     {
-        name : 'shape',
-        message : 'please select a shape',
-        type : 'list',
-        choices : ['circle', 'triangle', 'square'],
+      name: "shape",
+      message: "Please select a shape",
+      type: "list",
+      choices: ["circle", "triangle", "square"],
     },
     {
-        name : 'shapeColor',
-        message : 'please input a color for the shape',
-        type : 'input',
+      name: "shapeColor",
+      message: "Please select a color for the shape",
+      type: "list",
+      choices: function (answers) {
+        // Filter out the text color choice from the shape color choices
+        return [
+          "red",
+          "blue",
+          "green",
+          "yellow",
+          "orange",
+          "purple",
+          "black",
+          "white",
+        ].filter((color) => color !== answers.textColor);
+      },
     },
-])
-.then((answers) =>{
+  ])
+  .then((answers) => {
     console.log(answers);
-});
+    const { characters, textColor, shape, shapeColor } = answers;
+
+    let svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">`;
+
+    switch (shape) {
+      case "circle":
+        svgContent += `<circle cx="150" cy="100" r="50" fill="${shapeColor}"/>`;
+        break;
+      case "triangle":
+        svgContent += `<polygon points="150,50 100,150 200,150" fill="${shapeColor}"/>`;
+        break;
+      case "square":
+        svgContent += `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}"/>`;
+        break;
+    }
+
+    const textX = 150;
+    const textY = 120;
+    const textAnchor = "middle";
+
+    svgContent += `<text x="${textX}" y="${textY}" fill="${textColor}" text-anchor="${textAnchor}">${characters}</text>`;
+    svgContent += `</svg>`;
+
+    const folderPath = "./examples";
+    const timestamp = Date.now();
+    const fileName = `logo_${timestamp}.svg`;
+    const filePath = `${folderPath}/${fileName}`;
+
+    fs.writeFileSync(filePath, svgContent, "utf-8");
+    console.log("SVG created successfully");
+  });
+}
+
+module.exports = generateLogo;
+
+generateLogo();
+
+
